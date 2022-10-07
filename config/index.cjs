cc6578ae7445648fd0b99c9cd743329baa3cb9f1
DoraemonMo <115248331+DoraemonMo@users.noreply.github.com>
2022-10-07 22:20:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '槑',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQ-Hp56KVIgCRNBAV3LvPMYB-Jjc','oQ-Hp58FaxQLMGpiuJwl6ewBgVqM',
+      id: 'oQ-Hp56KVIgCRNBAV3LvPMYB-Jjc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'wIDsyZf5micuK3FiEAfSAQgATUsgM4hxGTWvC0PBBcM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
